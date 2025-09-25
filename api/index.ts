762c000153ea697485a91abfb2183cf3447cbb8f
Kyuhyun Kim <ggh163700@gmail.com>
2025-09-25 17:36:11 +0900

    CORS 설정을 특정 출처로 제한하고, 지원하는 HTTP 메서드를 명시하여 API의 보안성과 유연성을 개선함. 추가된 출처 목록을 통해 다양한 환경에서의 요청을 허용함.

--- api/index.ts
+++ api/index.ts
@@ -20,7 +20,19 @@ async function createApp(): Promise<NestExpressApplication> {
 
         const app = await NestFactory.create<NestExpressApplication>(AppModule);
         // CORS setup
-        // app.enableCors();
+        const ALLOW_ORIGINS = [
+            'https://lsso-admin.vercel.app',
+            'https://lsso-admin-git-dev-lumir-tech7s-projects.vercel.app',
+            'https://portal.lumir.space/',
+            'https://lsms.lumir.space/',
+            'http://localhost:3000',
+            // 필요하면 스테이징/프로덕션 도메인 추가
+        ];
+
+        app.enableCors({
+            origin: ALLOW_ORIGINS,
+            methods: 'GET,HEAD,POST,PATCH,PUT,DELETE,OPTIONS',
+        });
         // Global pipes
         app.useGlobalPipes(
             new ValidationPipe({
